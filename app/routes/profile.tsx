fef17f5948f121a241c277446b693ff4b1540de9
Jeff Emery <jemery@envisionradiology.com>
2022-09-12 14:26:24 -0600

    basic auth

--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -1,8 +1,8 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix</h1>
-      <a href="/login">Login</a>
+      <h1>Profile Route</h1>
+      <pre>This route is protected</pre>
     </div>
   )
 }
