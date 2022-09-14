3373d35899e709d6a0d5e2fda865196e43509729
Jeff Emery <jemery@envisionradiology.com>
2022-09-13 22:10:33 -0600

    links for auth actions

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,7 +2,15 @@ export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Welcome to Remix</h1>
-      <a href="/login">Login</a>
+      <p>
+        <a href="/about">About Route</a>
+      </p>
+      <p>
+        <a href="/profile">Profile Route (protected)</a>
+      </p>
+      <div>
+        <a href="/login">Login</a>
+      </div>
     </div>
   )
 }
