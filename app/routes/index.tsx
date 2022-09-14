b703a768032724c59b00f19ba3c24f6ff76a0e6d
Jeff Emery <jemery@envisionradiology.com>
2022-09-14 15:19:09 -0600

    bump github action

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Remix Beta 1</h1>
       <p>
         <a href="/about">About Route</a>
       </p>
